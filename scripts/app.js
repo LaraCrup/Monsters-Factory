051a8a7444cba5fc8ccc9bc010ad51f349866b09
LaraCrup <laracrup@gmail.com>
2023-09-22 18:42:12 -0300

    validaciones

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,124 @@
 Vue.component('forms-monster', {
     data: function(){
-        return {}
+        return {
+            verificar: {
+                nombre: false,
+                apodo: false,
+                profesion: false,
+                peso: false,
+                altura: false,
+                color: false,
+            },
+            monstruo: {
+                nombre:"",
+                apodo:"",
+                profesion:"",
+                peso:"",
+                altura:"",
+                color:"",
+            },
+            arrayMonstruos: [],
+        }
     },
     template: `
         <form v-on:submit.prevent>
             <h1>Creá tu monstruo aterrador</h1>
-            <div>
-                <label for="nombre">Nombre</label>
-                <input type="text" id="nombre">
-                <p>Ingrese un nombre</p>
-            </div>
-            <div>
-                <label for="apodo">Apodo</label>
-                <input type="text" id="apodo">
-                <p>Ingrese un apodo</p>
-            </div>
-            <div>
-                <label for="profesion">Profesión</label>
-                <input type="text" id="profesion">
-                <p>Ingrese una profesión</p>
-            </div>
-            <div>
-                <label for="peso">Peso</label>
-                <input type="number" id="peso">
-                <p>Ingrese un peso mayor a 0</p>
-            </div>
-            <div>
-                <label for="altura">Altura</label>
-                <input type="text" id="altura">
-                <p>Ingrese una altura mayor a 0</p>
-            </div>
-            <div>
-                <label for="color">Color</label>
-                <select id="color">
-                    <option value=""></option>
-                </select>
-                <p>Ingrese un color</p>
-            </div>
-            <atributos-monster></atributos-monster>
-            <div>
-                <button>Crear monstruo</button>
-            </div>
+            <fieldset>
+                <div>
+                    <label for="nombre">Nombre</label>
+                    <input type="text" id="nombre" v-model="monstruo.nombre">
+                    <p :class=" verificar.nombre ? 'mostrarError' : 'ocultarError' ">Ingrese un nombre</p>
+                </div>
+                <div>
+                    <label for="apodo">Apodo</label>
+                    <input type="text" id="apodo" v-model="monstruo.apodo">
+                    <p :class=" verificar.apodo ? 'mostrarError' : 'ocultarError' ">Ingrese un apodo</p>
+                </div>
+                <div>
+                    <label for="profesion">Profesión</label>
+                    <input type="text" id="profesion" v-model="monstruo.profesion">
+                    <p :class="verificar.profesion ? 'mostrarError' : 'ocultarError' ">Ingrese una profesión</p>
+                </div>
+                <div>
+                    <label for="peso">Peso</label>
+                    <input type="number" id="peso" v-model="monstruo.peso">
+                    <p :class=" verificar.peso ? 'mostrarError' : 'ocultarError' ">Ingrese un peso mayor a 0</p>
+                </div>
+                <div>
+                    <label for="altura">Altura</label>
+                    <input type="text" id="altura" v-model="monstruo.altura">
+                    <p :class="verificar.altura ? 'mostrarError' : 'ocultarError' ">Ingrese una altura mayor a 0</p>
+                </div>
+                <div>
+                    <label for="color">Color</label>
+                    <select id="color">
+                        <option value="verde">Verde</option>
+                        <option value="azul">Azul</option>
+                        <option value="violeta">Violeta</option>
+                    </select>
+                    <p>Ingrese un color</p>
+                </div>
+                <atributos-monster></atributos-monster>
+                <div>
+                    <button @click="validarCampos()" id="btnEnviar">Crear monstruo</button>
+                </div>
+            </fieldset>
         </form>
-    `
-})
+    `,
+    methods:{
+        validarCampos(){
+            if(this.monstruo.nombre === ""){
+                this.verificar.nombre = true;
+            }else {
+                this.verificar.nombre = false;
+                if(this.monstruo.apodo === ""){
+                    this.verificar.apodo = true;
+                }else {
+                    this.verificar.apodo = false;
+                    if(this.monstruo.profesion === ""){
+                        this.verificar.profesion = true;
+                    }else {
+                        this.verificar.profesion = false;
+                        if(this.monstruo.peso <= 0){
+                            this.verificar.peso = true;
+                        }else {
+                            this.verificar.peso = false;
+                            if(this.monstruo.altura <= 0){
+                                this.verificar.altura = true;
+                            }else {
+                                this.verificar.altura = false;
+                                this.enviarFormulario()
+                            };
+                        }
+                    }
+                }
+            }
+        },
+
+        enviarFormulario(){
+            this.arrayMonstruos.push(this.monstruo);
+            console.log(this.arrayMonstruos);
+            this.monstruo.nombre = "";
+            this.monstruo.apodo = "";
+            this.monstruo.profesion = "";
+            this.monstruo.peso = "";
+            this.monstruo.altura = "";
+        },
+
+        
+//   enviarFormulario() {
+//     // Agrega el objeto monstruo al array
+//     this.arrayMonstruos.push({ ...this.monstruo });
+//     console.log(this.arrayMonstruos);
+
+//     // Reinicia el objeto monstruo y los campos del formulario
+//     this.monstruo.nombre = "";
+//     this.monstruo.apodo = "";
+//     this.monstruo.profesion = "";
+//     this.monstruo.peso = "";
+//     this.monstruo.altura = "";
+    }
+    })
 
 Vue.component('atributos-monster', {
     data:function(){
@@ -92,5 +167,10 @@ var app = new Vue({
     el: '.container',
     data: {
 
+    },
+    methods:{
+        // mostrarMonstruoActual(){
+            
+        // }
     }
 })
