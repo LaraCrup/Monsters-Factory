82bc93a63b928708f76deef85070c535380ae7b8
LaraCrup <laracrup@gmail.com>
2023-09-22 23:03:05 -0300

    viernes 22

--- scripts/app.js
+++ scripts/app.js
@@ -23,55 +23,53 @@ Vue.component('forms-monster', {
     template: `
         <div class="forms-display">
             <form v-on:submit.prevent>
-                <fieldset>
-                    <div class="gridForms">
-                        <div>
-                            <label for="nombre">Nombre</label>
-                            <input type="text" id="nombre" v-model="monstruo.nombre">
-                            <p :class=" verificar.nombre ? 'mostrarError' : 'ocultarError' ">Ingrese un nombre</p>
-                        </div>
-                        <div>
-                            <label for="apodo">Apodo</label>
-                            <input type="text" id="apodo" v-model="monstruo.apodo">
-                            <p :class=" verificar.apodo ? 'mostrarError' : 'ocultarError' ">Ingrese un apodo</p>
-                        </div>
-                        <div>
-                            <label for="profesion">Profesión</label>
-                            <input type="text" id="profesion" v-model="monstruo.profesion">
-                            <p :class="verificar.profesion ? 'mostrarError' : 'ocultarError' ">Ingrese una profesión</p>
-                        </div>
-                        <div>
-                            <label for="peso">Peso</label>
-                            <input type="number" id="peso" v-model="monstruo.peso">
-                            <p :class=" verificar.peso ? 'mostrarError' : 'ocultarError' ">Ingrese un peso mayor a 0</p>
-                        </div>
-                        <div>
-                            <label for="altura">Altura</label>
-                            <input type="text" id="altura" v-model="monstruo.altura">
-                            <p :class="verificar.altura ? 'mostrarError' : 'ocultarError' ">Ingrese una altura mayor a 0</p>
-                        </div>
-                        <div>
-                            <label for="color">Color</label>
-                            <select id="color">
-                                <option value="verde">Verde</option>
-                                <option value="azul">Azul</option>
-                                <option value="violeta">Violeta</option>
-                            </select>
-                            <p>Ingrese un color</p>
-                        </div>
-                    </div>
-                    <atributos-monster></atributos-monster>
+                <div class="gridForms">
                     <div>
-                        <button @click="validarCampos(monstruo)">Crear monstruo</button>
+                        <label for="nombre">Nombre</label>
+                        <input type="text" id="nombre" v-model="monstruo.nombre">
+                        <p :class=" verificar.nombre ? 'mostrarError' : 'ocultarError' ">Ingrese un nombre</p>
                     </div>
-                </fieldset>
+                    <div>
+                        <label for="apodo">Apodo</label>
+                        <input type="text" id="apodo" v-model="monstruo.apodo">
+                        <p :class=" verificar.apodo ? 'mostrarError' : 'ocultarError' ">Ingrese un apodo</p>
+                    </div>
+                    <div>
+                        <label for="profesion">Profesión</label>
+                        <input type="text" id="profesion" v-model="monstruo.profesion">
+                        <p :class="verificar.profesion ? 'mostrarError' : 'ocultarError' ">Ingrese una profesión</p>
+                    </div>
+                    <div>
+                        <label for="peso">Peso</label>
+                        <input type="number" id="peso" v-model="monstruo.peso">
+                        <p :class=" verificar.peso ? 'mostrarError' : 'ocultarError' ">Ingrese un peso mayor a 0</p>
+                    </div>
+                    <div>
+                        <label for="altura">Altura</label>
+                        <input type="text" id="altura" v-model="monstruo.altura">
+                        <p :class="verificar.altura ? 'mostrarError' : 'ocultarError' ">Ingrese una altura mayor a 0</p>
+                    </div>
+                    <div>
+                        <label for="color">Color</label>
+                        <select id="color">
+                            <option value="verde">Verde</option>
+                            <option value="azul">Azul</option>
+                            <option value="violeta">Violeta</option>
+                        </select>
+                        <p>Ingrese un color</p>
+                    </div>
+                </div>
+                <atributos-monster></atributos-monster>
+                <div>
+                    <button @click="validarCampos(monstruo)">Crear monstruo</button>
+                </div>
             </form>
-            <div>
-                <p> {{this.monstruo.nombre}} </p>
-                <p> {{this.monstruo.apodo}} </p>
-                <p> {{this.monstruo.profesion}} </p>
-                <p> {{this.monstruo.peso}} </p>
-                <p> {{this.monstruo.altura}}</p>
+            <div class="cardPrincipal">
+                <p> {{this.monstruo.nombre | uppercase}} </p>
+                <p> {{this.monstruo.apodo | capitalize}} </p>
+                <p> {{this.monstruo.profesion | capitalize}} </p>
+                <p> {{this.monstruo.peso}}kg</p>
+                <p> {{this.monstruo.altura}}cm</p>
             </div>
         </div>
     `,
@@ -109,11 +107,6 @@ Vue.component('forms-monster', {
             this.arrayMonstruos.push(monstruo);
             this.monstruo = {nombre:"", apodo:"", profesion:"", peso:"", altura:""};
             console.log(this.arrayMonstruos);
-            // this.monstruo.nombre = "";
-            // this.monstruo.apodo = "";
-            // this.monstruo.profesion = "";
-            // this.monstruo.peso = "";
-            // this.monstruo.altura = "";
         },
 
     }
@@ -125,14 +118,14 @@ Vue.component('atributos-monster', {
     },
     template: `
         <section>
-        <div>
-            <h2>Elige los atributos de tu monstruo</h2>
             <div>
-                <span>15</span>
+                <h2>Elige los atributos de tu monstruo</h2>
+                <div>
+                    <span>15</span>
+                </div>
             </div>
-        </div>
-        <p>Tienes 15 puntos para distribuir entre los atributos de tu monstruo. Recuerda que si no utilizas todos los puntos ¡tu monstruo será menos poderoso!</p>
-        <div>
+                <p>Tienes 15 puntos para distribuir entre los atributos de tu monstruo. Recuerda que si no utilizas todos los puntos ¡tu monstruo será menos poderoso!</p>
+            <div>
             <div>
                 <p>Fuerza</p>
                 <div>
@@ -157,8 +150,7 @@ Vue.component('atributos-monster', {
                     <button>+</button>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     `
 })
 
